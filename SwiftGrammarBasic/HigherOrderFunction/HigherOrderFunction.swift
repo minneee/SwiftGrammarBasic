a699f71af411fc94d2eddc4529d8ca811fc6b203
minneee <alsgml022100@naver.com>
2025-08-21 10:20:14 +0900

    [chore] 문제풀이 파일 class로 감싸기

--- SwiftGrammarBasic/HigherOrderFunction/HigherOrderFunction.swift
+++ SwiftGrammarBasic/HigherOrderFunction/HigherOrderFunction.swift
@@ -5,21 +5,28 @@
 //  Created by 김민희 on 8/21/25.
 //
 
-// 1
-let numbers = [1, 2, 3, 4, 5]
-let mapResult: [String] = numbers.map { String($0) }
-
-// 2
-let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let stringEvenNumbers = numbers2.filter { $0 % 2 == 0 }.map { String($0) }
-
-// 3
-func myMap(_ numbers: [Int], _: (Int) -> String) -> [String] {
-  var result: [String] = []
-
-  for number in numbers {
-    result.append(String(number))
+class HigherOrderFunction {
+  // 1
+  let numbers = [1, 2, 3, 4, 5]
+  var mapResult: [String] {
+    numbers.map { String($0) }
   }
 
-  return result
+  // 2
+  let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  var stringEvenNumbers: [String] {
+    numbers2.filter { $0 % 2 == 0 }.map { String($0) }
+  }
+
+  // 3
+  func myMap(_ numbers: [Int], _: (Int) -> String) -> [String] {
+    var result: [String] = []
+
+    for number in numbers {
+      result.append(String(number))
+    }
+
+    return result
+  }
+
 }
--- SwiftGrammarBasic/main.swift
+++ SwiftGrammarBasic/main.swift
@@ -8,16 +8,22 @@
 import Foundation
 
 //MARK: -  필수 문제 1 (Closure)
-//print(sum(10, 20))
-//
-//calculate(x: 30, y: 10) {
-//  return "두 수의 차는 \($0 - $1) 입니다"
-//}
+let closure = Closure()
+
+print(closure.sum(10, 20))
+
+closure.calculate(x: 30, y: 10) {
+  return "두 수의 차는 \($0 - $1) 입니다"
+}
 
 //MARK: - 필수문제 2 (HigherOrderFunction)
-print(mapResult)
-print(stringEvenNumbers)
-let myMapResult = myMap([1, 2, 3, 4, 5]) {
+let higherOrderFunction = HigherOrderFunction()
+
+print(higherOrderFunction.mapResult)
+
+print(higherOrderFunction.stringEvenNumbers)
+
+let myMapResult = higherOrderFunction.myMap([1, 2, 3, 4, 5]) {
   String($0)
 }
 print(myMapResult)
