805cd348a69b14003b98cfd27c77405ea2903d93
김민희 <86248191+minneee@users.noreply.github.com>
2025-08-22 09:33:34 +0900

    [feature] 필수문제 1 구현 (#1)

---
+++ SwiftGrammarBasic/Closure/Closure.swift
@@ -0,0 +1,16 @@
+//
+//  Closure.swift
+//  SwiftGrammarBasic
+//
+//  Created by 김민희 on 8/20/25.
+//
+
+
+let sum: (Int, Int) -> String = {
+  return "두 수의 합은 \($0 + $1) 입니다"
+}
+
+func calculate(x: Int, y: Int, operation: (Int, Int) -> String) {
+  let result = operation(x, y)
+  print(result)
+}
